b722fd00b2430b0f0963ef34388f886988ad678b
Артём Несмеянов <tema.nesmeyanow@yandex.ru>
2024-10-03 13:06:31 +0300

    Кол-во дней и вычисление кол-ва секунд

--- time.pas
+++ time.pas
@@ -1,6 +1,12 @@
 ﻿##
 var year := ReadInteger('Введите год:');
 if (year mod 4 = 0) and (year mod 400 <> 0) then
-  print('Високосный')
+begin
+  print('Високосный');
+  print('366 дней');
+end;
 else
+begin
   print('Не високосный');
+  print('365 дней');
+end;
---
+++ timer.pas
@@ -0,0 +1,5 @@
+﻿##
+var h:= ReadInteger('Введите кол-во часов:');
+var m:= ReadInteger('Введите кол-во минут:');
+var s:= ReadInteger('Введите кол-во секунд:');
+print(h*3600+m*60+s)
